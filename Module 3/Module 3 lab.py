e6d2bd84b8282d93c022f146ddd8a4624337e823
akidanemariam <akidanemariam@my.nl.edu>
2024-07-19 13:41:27 -0500

    Module 3 lab

---
+++ Module 3/Module 3 lab.py
@@ -0,0 +1,10 @@
+#Abraham Kidanemariam
+#7/19/2024
+# Problem 1 - prints “Hello World” to the screen
+print ("Hello World")
+
+# Problem 2 - asks the user for their name and greets them with their name
+x = input ("Please enter your name: ")
+print ("Good morning " + x)
+
+# Problem 3 - only the users you and your instructor are greeted with their names
